2e94bfdb666c28c1e08910821a9efeb333f92702
mouryar <mourya.rajala@gmail.com>
2018-05-05 14:25:50 -0700

    Adding burger component and CSS

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React,{Component} from 'react';
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component{
     render(){
         return(
             <div>
                 <div>Burger</div>
-                <div>Burger Builder</div>
+                <Burger/>
             </div>
         );
     }
