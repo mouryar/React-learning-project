94fab2ca775c4d9f59bc5b3d3701143b8cc6f270
mouryar <mourya.rajala@gmail.com>
2018-05-05 21:09:17 -0700

    Adding tool bar to the application

--- src/components/UI/Navigation/Toolbar/Toolbar.js
+++ src/components/UI/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import classes from './Toolbar.css'
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const Toolbar = (props) => {
     return(
         <header className={classes.Toolbar}>
             <div>Menu</div>
-            <div>Logo</div>
+            <Logo></Logo>
             <nav>
-                ...
+                <NavigationItems/>
             </nav>
         </header>
     );
